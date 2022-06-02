15ee2de394f7e4271c931da86575e3cf982d144f
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-06-02 17:40:27 -0500

    All tables properly created and loaded

--- schema.sql
+++ schema.sql
@@ -44,8 +44,7 @@ CREATE TABLE titles (
   title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 -- Creating the department employees table for PH-EmployeeDB
@@ -55,8 +54,7 @@ CREATE TABLE dept_emp (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 SELECT * FROM departments;
