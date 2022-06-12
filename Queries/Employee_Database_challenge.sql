77d9762923dde0b20cb1fc45abe741f2d91f7c1a
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-06-12 17:28:40 -0500

    Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -23,9 +23,28 @@ FROM retirement_titles
 WHERE (to_date = '9999-01-01')
 ORDER BY emp_no, to_date DESC;
 
--- retrieve the number of employees by their most recent job title who are about to retire
+-- Retrieve the number of employees by their most recent job title who are about to retire
 SELECT COUNT(emp_no), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+-- Deliverable 2
+SELECT DISTINCT ON (emp_no)
+    e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date, 
+    de.from_date, 
+    de.to_date,
+    ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
